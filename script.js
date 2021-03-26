3240ae7cbb7aa7763a9cab0dac512220e5ace01b
fehmi-enes-eren <76635233+fehmi-enes-eren@users.noreply.github.com>
2021-03-26 12:09:58 -0700

    logic has changed all

--- script.js
+++ script.js
@@ -1,66 +1,5 @@
-const movies = {
-    action: {
-        imdb8: [["The Dark Knight",2008,"9.0","https://www.imdb.com/title/tt0468569/?ref_=adv_li_tt"],["The Lord of the Rings",2003,"8.9","https://www.imdb.com/title/tt0167260/?ref_=adv_li_tt"],["The Matrix",1999,"8.7","https://www.imdb.com/title/tt0133093/?ref_=adv_li_tt"]],
-        imdb7: [["Avengers Age of Ultron",2015,"7.3","https://www.imdb.com/title/tt2395427/?ref_=adv_li_tt"],["Thor Ragnarok",2017,"7.9","https://www.imdb.com/title/tt3501632/?ref_=adv_li_tt"],["The Gentlemen",2019,"7.8","https://www.imdb.com/title/tt8367814/?ref_=adv_li_tt"]],
-        imdb5: [["Gemini Man",2019,"5.7","https://www.imdb.com/title/tt1025100/?ref_=adv_li_tt"],["Birds of Prey",2020,"6.1","https://www.imdb.com/title/tt7713068/?ref_=adv_li_tt"],["News of the World",2020,"6.8","https://www.imdb.com/title/tt6878306/?ref_=adv_li_tt"],["Justice League",2017,"6.3","https://www.imdb.com/title/tt0974015/?ref_=adv_li_tt"]],
-        imdb4: [["The Marine",2006,"4.7","https://www.imdb.com/title/tt0419946/?ref_=adv_li_tt"],["Ra One",2011,"4.7","https://www.imdb.com/title/tt1562871/?ref_=adv_li_tt"],["Sadak 2",2020,"1.1","https://www.imdb.com/title/tt7886848/?ref_=adv_li_tt"],["Alone in the Dark",2005,"2.4","https://www.imdb.com/title/tt0369226/?ref_=adv_li_tt"]]
-    },
-    comedy: { 
-        imdb8: [["Parasite",2019,"8.6","https://www.imdb.com/title/tt6751668/?ref_=adv_li_tt"],["Deadpool",2016,"8,0","https://www.imdb.com/title/tt1431045/?ref_=adv_li_tt"],["The Grand Budapest Hotel",2014,"8.1","https://www.imdb.com/title/tt2278388/?ref_=adv_li_tt"],["Soul", 2020, "8.1", "https://www.imdb.com/title/tt2948372/?ref_=adv_li_tt"]],
-        imdb7: [["Once Upon a Time... In Hollywood",2019,"7,6","https://www.imdb.com/title/tt7131622/?ref_=adv_li_tt"],["Knives Out",2019,"7,9", "https://www.imdb.com/title/tt8946378/?ref_=adv_li_tt"], ["The Gentlemen", 2019,"7,8","https://www.imdb.com/title/tt8367814/?ref_=adv_li_tt"], ["Captain Fantastic", 2016, "7,9", "https://www.imdb.com/title/tt3553976/?ref_=adv_li_tt"]],
-        imdb5:[["The SpongeBob Movie Sponge on the Run",2020, "6,0","https://www.imdb.com/title/tt4823776/?ref_=adv_li_tt"],["Police Academy", 1984,"6,7", "https://www.imdb.com/title/tt0087928/?ref_=adv_li_tt"], ["EuroTrip",2004,"6,6","https://www.imdb.com/title/tt0356150/?ref_=adv_li_tt"],["Blithe Spirit",2020,"5,4","https://www.imdb.com/title/tt10334148/?ref_=adv_li_tt" ]],
-        imdb4: [["We Can Be Heroes", 2020, "4,7","https://www.imdb.com/title/tt10600398/?ref_=adv_li_tt"],["Charlie's Angels",2019, "4,8","https://www.imdb.com/title/tt5033998/?ref_=adv_li_tt"],["Chick Fight",2020,"4,5","https://www.imdb.com/title/tt10944486/?ref_=adv_li_tt"],["Taxi",2004,"4,5","https://www.imdb.com/title/tt0316732/?ref_=adv_li_tt"]]
-    },
-    animation: {
-        imdb8: [["The Lion King",1994,"8.5","https://www.imdb.com/title/tt0110357/?ref_=adv_li_tt"],["Toy Story",1995,"8.3","https://www.imdb.com/title/tt0114709/?ref_=adv_li_tt"],["Up",2009,"8.2","https://www.imdb.com/title/tt1049413/?ref_=adv_li_tt"], ["Aladdin",1992, "8.0","https://www.imdb.com/title/tt0103639/?ref_=adv_li_tt"]],
-        imdb7: [["Toy Story 2",1999,"7.9","https://www.imdb.com/title/tt0120363/?ref_=adv_li_tt"],["How to Train Your Dragon",2014,"7.8","https://www.imdb.com/title/tt1646971/?ref_=adv_li_tt"],["Shrek",2001,"7.8","https://www.imdb.com/title/tt0126029/?ref_=adv_li_tt"],["Moana",2016,"7.6","https://www.imdb.com/title/tt3521164/?ref_=adv_li_tt"]],
-        imdb5: [["Surf's Up",2007,"6.7","https://www.imdb.com/title/tt0423294/?ref_=adv_li_tt"],["Over the Hedge",2006,"6.7","https://www.imdb.com/title/tt0327084/?ref_=adv_li_tt"],["The Secret Life of Peds", 2016,"6,5","https://www.imdb.com/title/tt2709768/?ref_=adv_li_tt"],["Happ Feet",2006,"6.4","https://www.imdb.com/title/tt0366548/?ref_=adv_li_tt"]],
-        imdb4:[["Garfield",2004,"5.0","https://www.imdb.com/title/tt0356634/?ref_=adv_li_tt"],["The Emoji Movie",2007,"3.3","https://www.imdb.com/title/tt4877122/?ref_=adv_li_tt"],["Meet the Spartans",2008,"2.8","https://www.imdb.com/title/tt1073498/?ref_=adv_li_tt"]]
-    },
-    horror: {
-        imdb8: [["Psycho", 1960, "8.5", "https://www.imdb.com/title/tt0054215/?ref_=adv_li_tt"],
-        ["The Shining", 1980, "8.4", "https://www.imdb.com/title/tt0081505/?ref_=adv_li_tt"],
-        ["Alien", 1979, "8.4", "https://www.imdb.com/title/tt0078748/?ref_=adv_li_tt"],
-        ["Tumbbad" , 2018, "8.3", "https://www.imdb.com/title/tt8239946/?ref_=adv_li_tt"]],
-        imdb7: [["Let The Right One In", 2008, "7.9", "https://www.imdb.com/title/tt1139797/?ref_=adv_li_tt"],
-        ["Shaun of the Dead", 2004, "7.9", "https://www.imdb.com/title/tt0365748/?ref_=adv_li_tt"],
-        ["Dawn of the Dead", 1978, "7.9", "https://www.imdb.com/title/tt0077402/?ref_=adv_li_tt"],
-        ["King Kong" , 1933, "7.9", "https://www.imdb.com/title/tt0024216/?ref_=adv_li_tt"]],
-        imdb5: [["The Call", 2013, "6.7", "https://www.imdb.com/title/tt1911644/?ref_=adv_li_tt"],
-        ["Altered States", 1980, "6.9", "https://www.imdb.com/title/tt0080360/?ref_=adv_li_tt"],
-        ["The House That Jack Built", 2018, "6.8", "https://www.imdb.com/title/tt4003440/?ref_=adv_li_tt"],
-        ["Housebound" , 2014, "6.8", "https://www.imdb.com/title/tt3504048/?ref_=adv_li_tt"]],
-        imdb4: [["Child's Play", 1975, "5.9", "https://www.imdb.com/title/tt8663516/?ref_=adv_li_tt"],
-        ["Vivarium", 2019, "5.8", "https://www.imdb.com/title/tt8368406/?ref_=adv_li_tt"],
-        ["High Life", 2018, "5.8", "https://www.imdb.com/title/tt4827558/?ref_=adv_li_tt"],
-        ["Jigsaw 1" , 2017, "5.8", "https://www.imdb.com/title/tt3348730/?ref_=adv_li_tt"]],
-    },
-    drama:{
-        imdb8: [["The Shawshank Redemption",1994,"9.3","https://www.imdb.com/title/tt0111161/?ref_=adv_li_tt"],["The Godfather",1972,"9.2","https://www.imdb.com/title/tt0068646/?ref_=adv_li_tt"],["12 Angry Men",1957,"9.0","https://www.imdb.com/title/tt0050083/?ref_=adv_li_tt"], ["Hababam Sinifi ",1975,"9.3","https://www.imdb.com/title/tt0252487/?ref_=adv_li_tt"]],
-        imdb7: [["Schindler's List",1993,"8.9","https://www.imdb.com/title/tt0108052/?ref_=adv_li_tt"], ["Forrest Gump",1994,"8.8","https://www.imdb.com/title/tt0109830/?ref_=adv_li_tt"], ["Interstellar",2014,"8.6","https://www.imdb.com/title/tt0816692/?ref_=adv_li_tt"], ["Saving Private Ryan ",1998,"8.6","https://www.imdb.com/title/tt0120815/?ref_=adv_li_tt"]],
-        imdb5:[["I'm Thinking of Ending Things",2020,"6.6","https://www.imdb.com/title/tt7939766/?ref_=adv_li_tt"],["Contagion",2011,"6.7","https://www.imdb.com/title/tt1598778/?ref_=adv_li_tt"],["Rebecca",2020,"6.0","https://www.imdb.com/title/tt2235695/?ref_=adv_li_tt"], ["The Lovely Bones",2009,"6.7","https://www.imdb.com/title/tt0380510/?ref_=adv_li_tt"]],
-        imdb4:[["Cuties",2020,"3.1","https://www.imdb.com/title/tt9196192/?ref_=adv_li_tt"],["The Room ",2003,"3.7","https://www.imdb.com/title/tt0368226/?ref_=adv_li_tt"],["The Butterfly Effect 2",2006,"4.5","https://www.imdb.com/title/tt0457297/?ref_=adv_li_tt"], ["In the Land of Blood and Honey ",2011,"4.5","https://www.imdb.com/title/tt1714209/?ref_=adv_li_tt"]]
-    },
-    fantasy: {
-        imdb8: [["The Green Mile", 1999, "8.6", "https://www.imdb.com/title/tt0038650/?ref_=adv_li_tt"],
-        ["It's a Wonderful Life", 1946, "8.6", "https://www.imdb.com/title/tt8368406/?ref_=adv_li_tt"],
-        ["Baahubali 2 The Conclusion", 2017, "8.2", "https://www.imdb.com/title/tt4849438/?ref_=adv_li_tt"],
-        ["Soul" , 2020, "8.1", "https://www.imdb.com/title/tt2948372/?ref_=adv_li_tt"]],
-        imdb7: [["Isle of Dogs", 2018, "7.9", "https://www.imdb.com/title/tt5104604/?ref_=adv_li_tt"],
-        ["The Man from Earth", 2007, "7.9", "https://www.imdb.com/title/tt0756683/?ref_=adv_li_tt"],
-        ["Life of Pi", 2012, "7.9", "https://www.imdb.com/title/tt0454876/?ref_=adv_li_tt"],
-        ["Harry Potter and the Prisoner of Azkaban" , 2004, "7.9", "https://www.imdb.com/title/tt0304141/?ref_=adv_li_tt"]],
-        imdb5: [["Yesterday 3", 2019, "6.8", "https://www.imdb.com/title/tt8079248/?ref_=adv_li_tt"],
-        ["Storks", 2016, "6.8", "https://www.imdb.com/title/tt4624424/?ref_=adv_li_tt"],
-        ["Odd Thomas", 2013, "6.8", "https://www.imdb.com/title/tt1767354/?ref_=adv_li_tt"],
-        ["The Imaginarium of Doctor Parnassus" , 2009, "6.8", "https://www.imdb.com/title/tt1054606/?ref_=adv_li_tt"]],
-        imdb4: [["Mortal Kombat", 1995, "5.8", "https://www.imdb.com/title/tt0113855/?ref_=adv_li_tt"],
-        ["Ice Age Collision Course", 2016, "5.7", "https://www.imdb.com/title/tt3416828/?ref_=adv_li_tt"],
-        ["Pan", 2015, "5.7", "https://www.imdb.com/title/tt3332064/?ref_=adv_li_tt"],
-        ["Chicken Little" , 2005, "5.7", "https://www.imdb.com/title/tt0371606/?ref_=adv_li_tt"]],
-    },
-}
 
+import movies from "./movies.js"
 
 let genre1 =document.getElementById("genre1");
 let genre2 =document.getElementById("genre2");
@@ -77,6 +16,8 @@ let results = document.getElementById("results");
 
 let spin = document.getElementById("spin");
 
+let warning = document.getElementById("warning")
+
 
 const waitFor = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
@@ -85,21 +26,72 @@ const array = [`<i class="fas fa-spinner"></i>`, `<i class="fas fa-spinner fa-ro
 
 
 const type = (param , point) => {
-
+    console.log(param);
+    console.log(point);
 
     return new Promise ( (resolve, reject) => {
         setTimeout( ()=> {
-            
-                let randomAction = movies[param][point];
-                //console.log(movies[param][point]);
-                randomAction.forEach( item => {
-                    let list = document.createElement("li");
-                    list.innerHTML=`${item[0]} <a href="${item[3]}" target="_blank"> <br> <img class="resultIMG" src= "./films/${param}/${point}/${item[0].split(/\s/).join('')}.jpg"> <br> </a> (${item[1]})`
+            let newArray = [];
+            let resultAll = [];
+                        
 
+            movies.forEach(element => {
+                if(param && point !== "zero"){
+                    if(element.genre === param){
+                        if(point !== "9" && point !== "5" && element.imdb>point-1 && element.imdb<point){
+                            console.log("element");
+                            newArray.push(element)
+                            return newArray
+                        } else if(point === "9" && element.imdb>8){
+                            newArray.push(element);
+                            console.log("element")
+                            return newArray 
+                        } else if (point === "5" && element.imdb<5){
+                            newArray.push(element);
+                            console.log("element")
+                            return newArray
+                        }
+                    } 
+                } else if(param){
+                    if(element.genre === param){
+                        newArray.push(element);
+                        console.log("element")
+                        return newArray
+                    }
+                } else if(point !== "zero"){
+                    if(point !== "9" && point !== "5" && element.imdb>point-1 && element.imdb<point){
+                        console.log("element");
+                        newArray.push(element)
+                        return newArray
+                    } else if(point === "9" && element.imdb>8){
+                        newArray.push(element);
+                        console.log("element")
+                        return newArray 
+                    } else if (point === "5" && element.imdb<5){
+                        newArray.push(element);
+                        console.log("element")
+                        return newArray
+                    }
+                } 
+            })
+
+            
+            console.log(resultAll);
+            console.log(newArray);
+
+            if(newArray.length > 0){
+                newArray.forEach( item => {
+                    let list = document.createElement("li");
+                    list.innerHTML=`${item.name} <a href="${item.url}" target="_blank"> <br> <img class="resultIMG" src= "./films/${item["name"].split(/\s/).join('')}.jpg"> <br> </a> ${item.year} (${item.imdb})`
+    
                     results.appendChild(list);
                 })
-                resolve("movies positive")
-                reject("something is wrong")
+            } else {
+                warning.innerHTML = "No matches found!"
+            }
+            
+            resolve("movies positive")
+            reject("something is wrong")
         },2500)
     })
 }
@@ -125,6 +117,8 @@ const spinFunc = (param)=> {
 submitButton.onclick = () => {
 
     results.innerHTML = "";
+    warning.innerHTML = "";
+
 
     let genre1Check =document.getElementById("genre1").checked;
     let genre2Check =document.getElementById("genre2").checked;
@@ -136,27 +130,27 @@ submitButton.onclick = () => {
     let parameter;
     let point = pointsSelect.value;
 
-    if(genre1Check && point !== "zero"){
+    if(genre1Check){
         parameter = genre1.value;
-    } else if(genre2Check && point !== "zero"){
+    } else if(genre2Check){
         parameter = genre2.value;
-    } else if(genre3Check && point !== "zero"){
+    } else if(genre3Check){
         parameter = genre3.value;
-    } else if(genre4Check && point !== "zero"){
+    } else if(genre4Check){
         parameter = genre4.value;
-    } else if(genre5Check && point !== "zero"){
+    } else if(genre5Check){
         parameter = genre5.value;
-    } else if(genre6Check && point !== "zero"){
+    } else if(genre6Check ){
         parameter = genre6.value;
     } else {
-        alert(`Please make both Selections!`);
+        parameter = false;
     }
 
 
     // ---- this is the IMPORTANT part. GIVING A PARAMETER!!!
     
     // console.log(parameter, point);
-    if(parameter){
+    if(parameter || point !== "zero"){
         type(parameter, point)
         .then( (res)=> {console.log(res)})
         .catch( (err) => {console.log(err)});
@@ -173,6 +167,11 @@ submitButton.onclick = () => {
         .then ( ()=> {return waitFor(500)})
         .then ( () => {return spinFunc("")})
         .catch ( (err) => console.log(err));
+    }
+
+    if(parameter === false && point === "zero"){
+        warning.innerHTML = "Please, make at least one selection!";
+        console.log("afdasffasdf")
     }
 
 
