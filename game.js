767b2edc09b41135ead143a43ede3dbc234361e8
fehmi-enes-eren <76635233+fehmi-enes-eren@users.noreply.github.com>
2021-04-01 16:59:50 -0700

    game tested

--- game.js
+++ game.js
@@ -9,7 +9,13 @@ let resetBtn = document.getElementById("reset");
 // let yourRights = 0;
 // resultsPart.innerHTML = `Your score = ${yourRights} / ${total5}`;
 
-let data;
+let data = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "f7b90963eamshf47dc9bcd5e7ff5p1b0d21jsna5168b2db105",
+    "x-rapidapi-host": "imdb8.p.rapidapi.com",
+  },
+};
 // Use your own API key from -- https://rapidapi.com/apidojo/api/imdb8 ;
 
 /* 
